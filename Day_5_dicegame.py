b87440d0572b1a23174230739f64b89ef5782206
Shivank6298 <116372790+Shivank6298@users.noreply.github.com>
2022-10-25 11:22:56 +0530

    Add files via upload

---
+++ Day_5_dicegame.py
@@ -0,0 +1,15 @@
+#dice game
+import random
+min_val=1
+max_val=6
+
+roll_again="yes"
+
+while roll_again=="yes" or roll_again=="y":
+    print("rolling dice...... ")
+    print("the values are: ")
+    print(random.randint(min_val,max_val))
+    print(random.randint(min_val,max_val))
+
+    print("type y or yes to roll again or n for quit")
+    roll_again=input("roll the dice again? ")
