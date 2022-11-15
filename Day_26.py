d68c327836bc3c66b6c6cce2a0e816adbded86f0
Shivank6298 <116372790+Shivank6298@users.noreply.github.com>
2022-11-15 20:42:26 +0530

    Add files via upload

---
+++ Day_26.py
@@ -0,0 +1,8 @@
+#exception handling 
+
+a=["python","exception","try and except"]
+try:
+    for i in range(4):
+        print("the index and element of the array is ",i,a[i])
+except:
+        print("index out of range") 
