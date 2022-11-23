844763daa7e0452a235dd9b5e13e1495a41d5620
Shivank6298 <116372790+Shivank6298@users.noreply.github.com>
2022-11-23 20:10:40 +0530

    Add files via upload

---
+++ day_34.py
@@ -0,0 +1,11 @@
+#Q4. pythom program to remove punctuation from string
+
+punctuation = '''''!()-[]{};:'"\,<>./?@#$%^&*_~'''  
+my_str = input("Enter a string: ")  
+no_punct = ""  
+for char in my_str:  
+   if char not in punctuation:  
+       no_punct = no_punct + char  
+
+print(no_punct)
+
